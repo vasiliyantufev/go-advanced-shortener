57cc830320914fef9a38dabd8a1ec329f6e522fe
vasiliyantufev <vasiliyantufev@gmail.com>
2022-12-25 00:03:46 +0300

    cleaning

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -31,7 +31,6 @@ func main() {
 	rtr.HandleFunc("/", PostHandler).Methods("POST")
 	rtr.HandleFunc("/", index)
 	rtr.HandleFunc("/{id}", GetHandler).Methods("GET")
-	////rtr.HandleFunc("/{id:[0-9]+}", GetHandler).Methods("GET")
 
 	fmt.Printf("Starting application on port %v\n", portNumber)
 	http.ListenAndServe(portNumber, rtr)
@@ -46,59 +45,22 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	link := urls[short["id"]]
-	//loc := "http://" + r.Host + "/" + short["id"]
-
 	w.Header().Set("Location", link)
 	w.WriteHeader(http.StatusTemporaryRedirect)
-
-	//w.Header().Add("Location", link)
-	//w.Write([]byte(link))
-	//http.Redirect(w, r, link, http.StatusTemporaryRedirect)
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 
-	//var urls map[string]string
-
-	//resp, err := ioutil.ReadAll(r.Body)
 	resp, err := io.ReadAll(r.Body)
-
-	//w.Write([]byte(resp))
-	//var url Url
-
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	//json.Unmarshal(resp, &url)
-
 	short := shorting()
-
-	//urls[short] = url.URL
 	urls[short] = string(resp)
 
 	link := "http://" + r.Host + "/" + short
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(link))
-
-	//w.Write([]byte(par))
-	//w.Write([]byte(url.URL))
-
-	//пишем тело ответа
-	//w.Write([]byte(name.URL))
-	//fmt.Fprintln(w, par)
-
-	//url := r.FormValue("URL")
-	// обрабатываем ошибку
-	//if url == "" {
-	//	http.Error(w, "The url parameter is missing", http.StatusBadRequest)
-	//	return
-	//}
-	//
-	//urls = append(urls, string(url))
-	//w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	//w.WriteHeader(201)
-	//// пишем в тело ответа
-	//fmt.Fprintln(w, url)
 }
